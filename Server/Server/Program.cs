9cd3b9156c39cad1043e7c9e68951b6072319d39
MishiJeon <kevin.huang@ontariotechu.net>
2023-03-18 22:26:31 -0400

    Updated

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -7,7 +7,8 @@ public class TCPServer
 {
     private static byte[] buffer = new byte[512];
     private static Socket serverTCP, serverUDP;
-    private static Socket clientTCP, clientUDP;
+    private static Socket[] clientsTCP = new Socket[2];
+    private static Socket clientUDP;
     private static string sendMsg = "";
     private static float[] aPos;
 
@@ -28,10 +29,11 @@ public class TCPServer
 
         Console.WriteLine("Waiting for connections...");
 
-        // Accept a TCP connection
-        clientTCP = serverTCP.Accept();
-
-        Console.WriteLine("TCP client connected!");
+        // Accept two TCP connections
+        clientsTCP[0] = serverTCP.Accept();
+        Console.WriteLine("TCP client 1 connected!");
+        clientsTCP[1] = serverTCP.Accept();
+        Console.WriteLine("TCP client 2 connected!");
 
         // Receive data from UDP client
         EndPoint remoteClient = new IPEndPoint(IPAddress.Any, 0);
@@ -46,30 +48,36 @@ public class TCPServer
         // Send response to the UDP client
         serverUDP.SendTo(buffer, remoteClient);
 
-        // Loop to receive and send messages to the TCP client
+        // Loop to receive and send messages to the TCP clients
         while (true)
         {
-            // Receive data from the TCP client
-            byte[] clientMsgBuffer = new byte[512];
-            int receiveClientMessage = clientTCP.Receive(clientMsgBuffer);
+            // Receive data from the TCP clients
+            byte[] client1MsgBuffer = new byte[512];
+            int receiveClient1Message = clientsTCP[0].Receive(client1MsgBuffer);
+            byte[] client2MsgBuffer = new byte[512];
+            int receiveClient2Message = clientsTCP[1].Receive(client2MsgBuffer);
 
-            Console.WriteLine("Client message: {0}", Encoding.ASCII.GetString(clientMsgBuffer, 0, receiveClientMessage));
+            Console.WriteLine("Client 1 message: {0}", Encoding.ASCII.GetString(client1MsgBuffer, 0, receiveClient1Message));
+            Console.WriteLine("Client 2 message: {0}", Encoding.ASCII.GetString(client2MsgBuffer, 0, receiveClient2Message));
 
             // Get input from the console as a string
             Console.Write("\nEnter message: ");
             string userInput = Console.ReadLine();
 
-            // Convert the input string to bytes and send to the TCP client
+            // Convert the input string to bytes and send to the TCP clients
             byte[] message = Encoding.ASCII.GetBytes(userInput);
-            clientTCP.Send(message);
+            clientsTCP[0].Send(message);
+            clientsTCP[1].Send(message);
 
             // End loop
             break;
         }
 
         // Close the sockets and clean up
-        clientTCP.Shutdown(SocketShutdown.Both);
-        clientTCP.Close();
+        clientsTCP[0].Shutdown(SocketShutdown.Both);
+        clientsTCP[0].Close();
+        clientsTCP[1].Shutdown(SocketShutdown.Both);
+        clientsTCP[1].Close();
         serverTCP.Close();
         serverUDP.Close();
     }
